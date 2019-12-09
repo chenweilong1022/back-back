929d308cd84c0e8fb493831e6f7e68448e652712
chenweilong <chenweilong>
2019-12-09 15:27:07 +0800

    spread代码删除

--- ozygod-pm-model/src/main/java/com/ozygod/model/zdspread/dao/RecordExchangeDiamondEntityMapper.java
+++
@@ -1,9 +0,0 @@
-package com.ozygod.model.zdspread.dao;
-
-import com.ozygod.model.zdspread.entity.RecordExchangeDiamondEntity;
-
-public interface RecordExchangeDiamondEntityMapper {
-    int insert(RecordExchangeDiamondEntity record);
-
-    int insertSelective(RecordExchangeDiamondEntity record);
-}
