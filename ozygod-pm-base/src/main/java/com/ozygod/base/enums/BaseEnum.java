f736eb9aa1057358086ac6092e3d6ecf2231c0d2
chenweilong <chenweilong>
2019-12-17 13:43:30 +0800

    统一枚举实现接口

---
+++ ozygod-pm-base/src/main/java/com/ozygod/base/enums/BaseEnum.java
@@ -0,0 +1,16 @@
+package com.ozygod.base.enums;
+
+/**
+ * 统一枚举实现接口
+ * @author chenweilong
+ * @email 1433471850@qq.com
+ * @date 2019/6/18
+ */
+public interface BaseEnum {
+
+
+    Integer getKey();
+
+    String getValue();
+
+}
