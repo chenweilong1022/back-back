390e8206df2fbaaa15283f87187aa051eeb5e9f6
chenweilong <chenweilong>
2019-12-17 13:43:09 +0800

    rediskey 枚举

---
+++ ozygod-pm-base/src/main/java/com/ozygod/base/enums/RedisKeys.java
@@ -0,0 +1,43 @@
+package com.ozygod.base.enums;
+
+import com.ozygod.base.utils.EnumUtil;
+import lombok.Getter;
+
+/**
+ * rediskey 枚举
+ * @author chenweilong
+ * @email 1433471850@qq.com
+ * @date 2019/6/17
+ */
+@Getter
+public enum RedisKeys implements BaseEnum {
+
+    // 总兑换数的key
+    ONE(1,"platform:totalBonus");
+
+
+    private Integer key;
+    private String value;
+
+    public static final String DESCRIBE = "1:不是 2:是 ";
+
+
+    /**
+     * 内部接口,实现该接口可获得中文描述
+     */
+    public interface RedisKeysStr {
+
+        Integer getRedisKeys();
+
+
+        default String getRedisKeysStr() {
+            return EnumUtil.queryValueByKey(getRedisKeys(), IndexCard.values());
+        }
+    }
+
+    RedisKeys(Integer key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+}
