f5c443d58ecb99282e2d3821b4153ebed2afc3eb
chenweilong <chenweilong>
2019-12-17 13:46:52 +0800

    枚举工具类

---
+++ ozygod-pm-base/src/main/java/com/ozygod/base/utils/EnumUtil.java
@@ -0,0 +1,37 @@
+package com.ozygod.base.utils;
+
+import cn.hutool.core.util.ObjectUtil;
+import com.ozygod.base.enums.BaseEnum;
+import com.ozygod.base.enums.IndexCard;
+import com.ozygod.base.vo.EnumVo;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 枚举工具类
+ */
+public class EnumUtil {
+
+
+    public static void main(String[] args) {
+        System.out.println(enumToVo(IndexCard.values()));
+    }
+
+
+    public static List<EnumVo> enumToVo(BaseEnum[] baseEnums) {
+        return Arrays.stream(baseEnums).map(EnumUtil::toEnumVo).collect(Collectors.toList());
+    }
+
+    public static String queryValueByKey(Integer key, BaseEnum[] baseEnums) {
+        BaseEnum baseEnum1 = Arrays.stream(baseEnums).filter(baseEnum -> baseEnum.getKey().equals(key)).findAny().orElse(null);
+        return ObjectUtil.isNotNull(baseEnum1) ? baseEnum1.getValue() : null;
+    }
+
+
+    private static EnumVo toEnumVo(BaseEnum baseEnum) {
+        return new EnumVo().setKey(baseEnum.getKey()).setValue(baseEnum.getValue());
+    }
+
+}
