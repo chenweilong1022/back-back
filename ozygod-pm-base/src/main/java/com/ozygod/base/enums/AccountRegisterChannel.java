900a0ce9d3ae835b654910a30648fa34c24a0c9f
chenweilong <chenweilong>
2019-12-19 09:49:32 +0800

    渠道

---
+++ ozygod-pm-base/src/main/java/com/ozygod/base/enums/AccountRegisterChannel.java
@@ -0,0 +1,56 @@
+package com.ozygod.base.enums;
+
+import com.ozygod.base.utils.EnumUtil;
+import lombok.Getter;
+
+/**
+ * 渠道
+ * @author chenweilong
+ * @email 1433471850@qq.com
+ * @date 2019/6/17
+ */
+@Getter
+public enum AccountRegisterChannel implements BaseEnum {
+
+//    GUEST(1,"guest","快速登录"),
+//    OTHER(2,"非guest","手机登录");
+        C3521(5,"3521");
+//    SIX(6,"总充值成功率"),
+//    SEVEN(7,"兑换金额(元)"),
+//    EIGHT(8,"今日兑换率");
+
+
+    private Integer key;
+    private String value;
+
+    private String loginWay;
+
+
+    public static final String DESCRIBE = "1:不是 2:是 ";
+
+
+    /**
+     * 内部接口,实现该接口可获得中文描述
+     */
+    public interface AccountRegisterChannelStr {
+
+        Integer getAccountRegisterChannel();
+
+
+        default String getAccountRegisterChannelStr() {
+            return EnumUtil.queryValueByKey(getAccountRegisterChannel(), AccountRegisterChannel.values());
+        }
+    }
+
+    AccountRegisterChannel(Integer key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    AccountRegisterChannel(Integer key, String value, String loginWay) {
+        this.key = key;
+        this.value = value;
+        this.loginWay = loginWay;
+    }
+
+}
