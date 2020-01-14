9199f7d9f8225a74362dee4e094192f8dcc70e0f
chenweilong <chenweilong>
2020-01-14 10:07:32 +0800

    增加一个域名是否封禁的工具类

--- ozygod-pm-base/src/main/java/com/ozygod/base/utils/WeChatUtil.java
+++ ozygod-pm-base/src/main/java/com/ozygod/base/utils/WeChatUtil.java
@@ -2,7 +2,10 @@ package com.ozygod.base.utils;
 
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
 import com.ozygod.base.redis.StringRedisDao;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +13,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @title:
@@ -34,6 +39,8 @@ public class WeChatUtil {
      */
     private static final String accessTokenUrl = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s";
     private static final String long2ShortUrl = "https://api.weixin.qq.com/cgi-bin/shorturl?access_token=%s";
+
+    private static final String wxLongUrlKey = "wxKey%s";
     /**
      * accessToken缓存key
      */
@@ -72,5 +79,114 @@ public class WeChatUtil {
 
         return "";
     }
+
+
+    /**
+     * 获取微信短链接
+     * @param longUrl
+     * @return
+     */
+    public String getWeChatShortUrl1(String longUrl) {
+        // 先检查longUrl是否已经有对应的shortUrl
+//        String shortUrl = stringRedisDao.readStr(longUrl);
+//        if (!CommonUtil.isEmptyStr(shortUrl)) {
+//            return shortUrl;
+//        }
+        // 检查redis是否存在accessToken
+        String accessToken = "";
+        // 当accessToken不存在时，从微信拉取数据
+//        if (CommonUtil.isEmptyStr(accessToken)) {
+//            String url = ;
+            String result1 = HttpRequestUtil.sendGet(String.format(accessTokenUrl, appId, appSecret));
+            log.info("result:" + result1);
+//            if (!CommonUtil.isEmptyStr(result)) {
+                Map<String, Object> resultMap = JSON.parseObject(result1, new TypeReference<Map<String, Object>>(){});
+                accessToken = String.valueOf(resultMap.get("access_token"));
+                Integer expiresIn = (int)resultMap.get("expires_in");
+                // 获取到后，保存在redis中
+//                stringRedisDao.saveStr(accessTokenKey, accessToken, expiresIn);
+//            }
+//        }
+        // 根据长链接获取短链接
+//        String url = ;
+//        Map<String, String> paramMap = new HashMap<>();
+//        paramMap.put("action", "long2short");
+//        paramMap.put("long_url", longUrl);
+//        String result = HttpRequestUtil.sendPostJson(String.format(long2ShortUrl, accessToken), JSON.toJSONString(paramMap));
+//        log.info("result:" + result);
+//        if (!CommonUtil.isEmptyStr(result)) {
+//            Map<String, Object> resultMap = JSON.parseObject(result, new TypeReference<Map<String, Object>>(){});
+//            shortUrl = String.valueOf(resultMap.get("short_url"));
+//            String errmsg = String.valueOf(resultMap.get("errmsg"));
+//            if ("ok".equals(errmsg)) {
+//                // 获取到后，保存在redis中，保存24小时
+//                stringRedisDao.saveStr(longUrl, shortUrl, 3600 * 24);
+//                return shortUrl;
+//            }
+//        }
+//        return "";
+        return  "";
+    }
+
+    /**
+     * 判断长域名是否封禁
+     * @param longUrl
+     * @return
+     */
+    public boolean isBlocked(String longUrl) {
+        /**
+         * 获取微信token
+         */
+        String accessToken = stringRedisDao.readStr(accessTokenKey);
+        if (StrUtil.isBlank(accessToken)) {
+            String result = HttpUtil.get(String.format(accessTokenUrl, appId, appSecret));
+
+            if (StrUtil.isNotBlank(result)) {
+                Map<String, Object> resultMap = JSON.parseObject(result, new TypeReference<Map<String, Object>>(){});
+                accessToken = String.valueOf(resultMap.get("access_token"));
+                Integer expiresIn = (int)resultMap.get("expires_in");
+                // 获取到后，保存在redis中
+                stringRedisDao.saveStr(accessTokenKey, accessToken, expiresIn);
+            }
+        }
+        /**
+         * 判断redis是否存在
+         */
+        String shortUrl = stringRedisDao.readStr(String.format(wxLongUrlKey, longUrl));
+        /**
+         * 如果为空重新获取
+         */
+        if (StrUtil.isBlank(shortUrl)) {
+            /**
+             * 向微信发送转短链接
+             */
+            Map<String, String> paramMap = new HashMap<>();
+            paramMap.put("action", "long2short");
+            paramMap.put("long_url", longUrl);
+            String result = HttpRequestUtil.sendPostJson(String.format(long2ShortUrl,accessToken), JSON.toJSONString(paramMap));
+            log.info("result:" + result);
+            /**
+             * 判断是否为空不为空存到redis
+             */
+            if (StrUtil.isNotBlank(result)) {
+                Map<String, Object> resultMap = JSON.parseObject(result, new TypeReference<Map<String, Object>>(){});
+                shortUrl = String.valueOf(resultMap.get("short_url"));
+                String errmsg = String.valueOf(resultMap.get("errmsg"));
+                stringRedisDao.saveStr(String.format(wxLongUrlKey, longUrl),shortUrl,60 * 10);
+            }
+        }
+        /**
+         * 向短链接发送http请求
+         * 如果存在 res.wx.qq.com/a/wx110/webpage-intercept/res/js/chunk-vendors.bf498f79.js\\
+         * 说明域名被封
+         * 如果没有说明域名正常
+         */
+        String isWx110 = HttpRequestUtil.sendGet(shortUrl);
+        if (isWx110.contains("res.wx.qq.com/a/wx110/webpage-intercept/res/js/chunk-vendors.bf498f79.js")) {
+            return true;
+        }
+        return false;
+    }
+
 
 }
