96cd59080fba83ef95c2d273192f5b7511404901
chenweilong <chenweilong>
2020-07-28 16:29:31 +0800

    Maven指定阿里云仓库

--- restart.sh
+++ restart.sh
@@ -4,6 +4,7 @@
 rm -rf back-back
 
 docker run -i --rm -v ${HOME}:/root -v $(pwd):/git alpine/git clone https://chenweilong961022:ch1433471850@gitee.com/chenweilong961022/back-back.git
+docker run -i --rm -v ${HOME}:/root -v $(pwd):/git alpine/git clone https://chenweilong961022:ch1433471850@gitee.com/chenweilong961022/back-front.git
 
 docker run -i --rm --name my-maven-project -v "$(pwd)":/usr/src/mymaven -v maven-repo:/root/.m2 -w /usr/src/mymaven maven:3.3-jdk-8 mvn clean install -Dmaven.test.skip=true -f back-back/pom.xml
 
